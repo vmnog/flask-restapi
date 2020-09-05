07239b1543c5f2d9902bbb6acb0455d04ebe2edf
Victor Nogueira <vmnogueira.ti@gmail.com>
2020-09-04 23:20:53 -0300

    first commit

---
+++ app.py
@@ -0,0 +1,73 @@
+from flask import Flask, jsonify, request
+
+app = Flask(__name__)
+
+stores = [
+    {
+        'name': 'vmnog_store',
+        'items': [
+            {
+                'name': 'Item_1',
+                'price': 15.99
+            }
+        ]
+    }
+]
+
+
+# POST /store data: {name:}
+@app.route('/store', methods=['POST'])
+def create_store():
+    request_data = request.get_json()
+    new_store = {
+        'name': request_data['name'],
+        'items': []
+    }
+    stores.append(new_store)
+    return jsonify(new_store)
+
+
+# GET /store/<string:name>
+@app.route('/store/<string:name>')
+def get_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({"store": store})
+
+    return jsonify({"error": "store not found"})
+
+
+# GET /store/<string:name>
+@app.route('/store')
+def get_stores():
+    return jsonify({'stores': stores})
+
+
+# POST /store/<string:name/item {name:, price:}
+@app.route('/store/<string:name>/item', methods=['POST'])
+def create_item_in_store(name):
+    request_data = request.get_json()
+
+    for store in stores:
+        if store['name'] == name:
+            new_item = {
+                'name': request_data['name'],
+                'price': request_data['price']
+            }
+            store['items'].append(new_item)
+            return jsonify(store)
+
+    return jsonify({"error": "store not found"})
+
+
+# GET /store/<string:name>/item
+@app.route('/store/<string:name>/item')
+def get_item_in_store(name):
+    for store in stores:
+        if store['name'] == name:
+            return jsonify({"items": store['items']})
+
+    return jsonify({"error": "store not found"})
+
+
+app.run(port=3333)
